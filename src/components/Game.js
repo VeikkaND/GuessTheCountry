e79948696af18c4e824eb8a22da564d215cc2fc8
Veikka <veikka.nevala@outlook.com>
2023-06-27 14:28:56 +0300

    basing guesses

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,10 +2,13 @@ import { useState } from "react"
 import InfoBox from "./InfoBox"
 
 const Game = ({country}) => {
-    console.log(country)
+    console.log(country.name.common)
     const [getGuesses, setGuesses] = useState(0)
+    const [gameOver, setGameOver] = useState(false)
+    const [guess, setGuess] = useState("")
 
     const guesses = getGuesses
+    const correctAnswer = country.name.common.toLowerCase()
 
     const populationStyle = {display: "none"}
     const areaStyle = {display: "none"}
@@ -14,33 +17,105 @@ const Game = ({country}) => {
     
     const handleSubmit = (event) => {
         event.preventDefault()
-        setGuesses(guesses + 1)
+        if(guess.toLowerCase() === correctAnswer) {
+            setGameOver(true)
+        } else {
+            setGuesses(guesses + 1)
+            setGuess("")
+        }
     }
-    switch(guesses) {
-        case 0:
-            populationStyle.display = "block"
-            break;
-        case 1:
-            populationStyle.display = "block";
-            areaStyle.display = "block"
-            break;
-        case 2:
-            populationStyle.display = "block";
-            areaStyle.display = "block";
-            capitalStyle.display = "block";
-            break;
-        default:
-            populationStyle.display = "block";
-            areaStyle.display = "block";
-            capitalStyle.display = "block";
-            flagStyle.display = "block"
-            break;
+
+    const handleRestart = () => {
+        window.location.reload(false)
+    }
+
+    const handleChange = (event) => {
+        event.preventDefault()
+        if(!gameOver) setGuess(event.target.value)
+    }
+
+    const InputForm = () => {
+        return (
+            <form onSubmit={handleSubmit}>
+                <input autoFocus onChange={handleChange} value={guess}></input>
+            </form>
+        )
+    }
+
+    const GameOverInfo = () => {
+        return (
+            <div>
+                <InputForm />
+                <div className="clues">
+                    <InfoBox infoType={"Population"} 
+                        info={country.population}/>
+                    <InfoBox infoType={"Area"} 
+                        info={country.area}/>
+                    <InfoBox infoType={"Capital"} 
+                        info={country.capital[0]}/>
+                    <InfoBox infoType={"flag"} 
+                        info={country.flags.png}/>
+                </div>
+            </div>
+        )
+    }
+
+    const RestartButton = () => {
+        return (<button onClick={handleRestart}>Play again</button>)
+    }
+
+    if(!gameOver) {
+        switch(guesses) {
+            case 0:
+                populationStyle.display = "block"
+                break;
+            case 1:
+                populationStyle.display = "block";
+                areaStyle.display = "block"
+                break;
+            case 2:
+                populationStyle.display = "block";
+                areaStyle.display = "block";
+                capitalStyle.display = "block";
+                break;
+            case 3:
+                populationStyle.display = "block";
+                areaStyle.display = "block";
+                capitalStyle.display = "block";
+                flagStyle.display = "block"
+                break;
+            default:
+                setGameOver(true)
+                populationStyle.display = "block";
+                areaStyle.display = "block";
+                capitalStyle.display = "block";
+                flagStyle.display = "block"
+        }
+    }
+    
+    if(gameOver) {
+        if(guesses < 4) {
+            return (
+                <div>
+                    <GameOverInfo />
+                    <h3>Correct answer</h3>
+                    <h4>The country was {country.name.common}</h4>
+                    <RestartButton />
+                </div>
+            )
+        }
+        return (
+            <div>
+                <GameOverInfo />
+                <h3>Game Over</h3>
+                <h4>The correct country was {country.name.common}</h4>
+                <RestartButton />
+            </div>
+        )
     }
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <input></input>
-            </form>
+            <InputForm />
             <div className="clues">
                 <InfoBox infoType={"Population"} 
                     info={country.population} style={populationStyle}/>
@@ -49,7 +124,7 @@ const Game = ({country}) => {
                 <InfoBox infoType={"Capital"} 
                     info={country.capital[0]} style={capitalStyle}/>
                 <InfoBox infoType={"flag"} 
-                    info={country.flags.alt} style={flagStyle}/>
+                    info={country.flags.png} style={flagStyle}/>
             </div>
         </div>
     )
--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,4 +1,11 @@
 const InfoBox = ({infoType, info, style}) => {
+    if(infoType === "flag") {
+        return (
+            <div style={style}>
+                {infoType}: <img src={info}></img>
+            </div>
+        )
+    }
     return (
         <div style={style}>
             {infoType}: {info}
